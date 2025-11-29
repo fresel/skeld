9af53dc24c9390caf7c2677e1a997bcd4fde10cc
Fredrik Selander <fresel72@gmail.com>
2025-11-30 00:30:31 +0100

    Added API Server

---
+++ api-server/src/main/java/com/github/fresel/skeld/api/server/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.fresel.skeld.api.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
